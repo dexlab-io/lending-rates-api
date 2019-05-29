90430bd913c14bc2e72a795a4b10eec3f7008382
Aleix <nigeon@gmail.com>
2019-05-29 22:45:21 +0200

    bZx WIP

--- src/managers/bzx.ts
+++ src/managers/bzx.ts
@@ -1,5 +1,30 @@
+import { ethers } from "ethers";
+import * as fs from "fs";
+
 export default class Bzx {
+    public bzxTokenizedRegistryABI;
+    public bzxITokenABI;
+    public contractAddress = "0xd3a04ec94b32dc4edbfc8d3be4cd44850e6df246";
+
+    constructor() {
+        this.bzxTokenizedRegistryABI = JSON.parse(fs.readFileSync("./contracts/bzxTokenizedRegistryABI.json", "utf8"));
+        this.bzxITokenABI = JSON.parse(fs.readFileSync("./contracts/bzxITokenABI.json", "utf8"));
+    }
+
     public async getRates() {
-        return false;
+        const provider = ethers.getDefaultProvider("ropsten");
+
+        const bzxContract = new ethers.Contract(this.contractAddress, this.bzxTokenizedRegistryABI, provider);
+
+        const tokens = await bzxContract.getTokens(0, 10000, 1);
+
+        const rates = [];
+        for (const token of tokens) {
+            const tokenContract = new ethers.Contract(token.token, this.bzxITokenABI, provider);
+            const r = await tokenContract.supplyInterestRate();
+            rates.push({name: token.symbol, apr: r});
+        }
+
+        global.console.log(rates);
     }
 }
--- test/integration/bzx.int.ts
+++ test/integration/bzx.int.ts
@@ -4,6 +4,7 @@ import Bzx from "../../src/managers/bzx";
 describe("Bzx protocol", () => {
     it("It gets the actual rates", async () => {
         const bzx = new Bzx();
-        assert.isNotEmpty(bzx.getRates());
-    });
+        await bzx.getRates();
+        global.console.log("DONE");
+    }).timeout(15000);
 });
